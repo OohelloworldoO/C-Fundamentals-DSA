70dbe6ff7d2edb7a7c7fe29de3318a41af364802
OohelloworldoO <sunny1234860@gmail.com>
2026-05-12 05:05:44 +0800

    update

--- DataStructure/Queue/Queue.c
+++ DataStructure/Queue/Queue.c
@@ -1,61 +1,32 @@
-#include <stdio.h>
-#include <stdbool.h>
+#include<stdio.h>
+#include<stdlib.h>
 
-#define MAX_SIZE 100
+typedef struct Node
+{
+    int data;
+    struct Node *next;
+} Node;
 
-typedef struct {
-    int arr[MAX_SIZE];
-    int front;
-    int rear;
+typedef struct
+{
+    Node *front;
+    Node *rear;
 } Queue;
 
-void initialize(Queue *q)
+void initQueue(Queue *q)
 {
-    q->front = 0;
-    q->rear = -1;
-}
-
-bool isEmpty(Queue *q)
-{
-    return q->front > q->rear;
-}
-
-bool isFull(Queue *q)
-{
-    return q->rear == MAX_SIZE - 1;
+    q->front = NULL;
+    q->rear = NULL;
 }
 
 void enqueue(Queue *q, int value)
 {
-    if (isFull(q)) {
-        printf("Queue full\n");
-        return;
+    Node *newNode = (Node*)malloc(sizeof(Node));
+    newNode->data = value;
+    newNode->next = NULL;
+
+    if(q->rear == NULL)
+    {
+        q->front = q->rear = newNode;
     }
-
-    q->arr[++q->rear] = value;
-}
-
-int dequeue(Queue *q)
-{
-    if (isEmpty(q)) {
-        printf("Queue empty\n");
-        return -1;
-    }
-
-    return q->arr[q->front++];
-}
-
-int main()
-{
-    Queue q;
-    initialize(&q);
-
-    enqueue(&q, 10);
-    enqueue(&q, 20);
-    enqueue(&q, 30);
-
-    printf("%d\n", dequeue(&q)); // 10
-    printf("%d\n", dequeue(&q)); // 20
-    printf("%d\n", dequeue(&q)); // 30
-    return 0;
 }
